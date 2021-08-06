cae9f92f6a6129eae625df6bb407fd5056619f9c
rbroeder <88403153+rbroeder@users.noreply.github.com>
2021-08-06 10:13:07 -0300

    4

--- customActivity.js
+++ customActivity.js
@@ -93,11 +93,13 @@ define(["postmonger"], function (Postmonger) {
   function onGetTokens(tokens) {
     // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
     // console.log(tokens);
+    console.log(tokens);
   }
 
   function onGetEndpoints(endpoints) {
     // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
     // console.log(endpoints);
+    console.log(endpoints);
   }
 
   function onClickedNext() {
